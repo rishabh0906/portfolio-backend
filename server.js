b3ac46d8c3f739eb2fce5d42d36fabc06dabb03e
rishabh <rishabhj945@gmail.com>
2021-08-19 14:58:01 +0530

    changes

--- server.js
+++ server.js
@@ -1,59 +1,57 @@
 const express = require("express");
-require('dotenv').config();
+const { google } = require("googleapis");
+require("dotenv").config();
 const nodemailer = require("nodemailer");
-const path=require('path');
+const path = require("path");
 let app = express();
 
 
 app.use(express.json());
- app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
+let Oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+Oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
-
-let transporter = nodemailer.createTransport({
- service:process.env.USER_SERVICE,
-host:"smtp.gmail.com",
-  auth: {
-    user: process.env.USER_MAIL,
-    pass: process.env.USER_PWD,
-  },
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages!");
-  }
-});
-
-app.post("/access", (req, res, next) => {
-  var mail = {
+async function sendmail(obj) {
+  const ACCESS_TOKEN = await Oauth2Client.getAccessToken();
+  const transport = nodemailer.createTransport({
+    service: process.env.USER_SERVICE,
+    auth: {
+      type: "OAuth2",
+      user: "rishabhj945@gmail.com",
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: ACCESS_TOKEN,
+    },
+  });
+  const mail = {
     from: process.env.SENDER,
     to: process.env.RECIEVER,
     subject: "portfolio-suggest",
-    text: `viewer : ${req.body.name} \n email : ${req.body.email} \n text :${req.body.text} `,
+    text: `viewer : ${obj.name} \n email : ${obj.email} \n text :${obj.text}`,
   };
+  return transport.sendMail(mail);
+}
 
-  transporter.sendMail(mail, (err, info) => {
-    if (err) {
-      console.log(err);
-      res.json({
-        status: "fail",
-      });
-    } else {
-      res.json({
-        status: "success",
-      });
-    }
-  });
+app.post("/access", (req, res, next) => {
+  sendmail(req.body)
+    .then((info) => {
+      res.json({status:"success"});
+    })
+    .catch((err) => {
+      res.json({status:"fail"});
+    });
 });
-
-app.get('/*',(req,res)=>{
-
-  res.sendFile(path.join(__dirname,'build','index.html'))
+app.get("/*",(req,res)=>{
+  res.sendFile(path.join(__dirname,'build','index.html'));
 })
-const PORT = process.env.PORT ||5000;
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.info(`server has started on ${PORT}`);
